ae50dc75e780dac3610a2eb3fc557e0c04f2ff7f
Yolxi <yolxicuba@gmail.com>
2018-07-13 14:15:35 -0400

    simple change made in Yolxi-branch

--- GitExampleXC9/ViewController.swift
+++ GitExampleXC9/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print("some change")
+        print("some changemade in yolxi-brand")
     }
 
     override var representedObject: Any? {
